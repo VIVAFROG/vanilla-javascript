20ba08fc7c30c6b65e2d7a951b3031ebaf1c9f23
VIVAFROG <vivafrog@gmail.com>
2018-11-24 00:08:41 +0900

    1-6

--- Ex01/index.js
+++ Ex01/index.js
@@ -1,1 +1,4 @@
-console.log('Im Working. Im JS. Im Beautiful, Im worth it'); 
+console.log("Im Working. Im JS. Im Beautiful, Im worth it");
+const a = 221;
+let b = a - 5;
+console.log(b, a);
---
+++ readme.md
@@ -0,0 +1,4 @@
+# vanilla-javascript
+
+var : 변수
+let : 상수
